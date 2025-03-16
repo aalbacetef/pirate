8976b5752ac1f7da043c3b321f211c47e3235896
Arturo Albacete <aalbacetef@gmail.com>
2025-03-16 15:10:04 +0100

    linter: wrap errors

--- safebuffer.go
+++ safebuffer.go
@@ -2,6 +2,7 @@ package pirate
 
 import (
 	"bytes"
+	"fmt"
 	"sync"
 )
 
@@ -18,7 +19,12 @@ func (b *safeBuffer) Read(d []byte) (int, error) {
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 
-	return b.buf.Read(d)
+	n, err := b.buf.Read(d)
+	if err != nil {
+		return n, fmt.Errorf("could not read from buffer: %w", err)
+	}
+
+	return n, nil
 }
 
 func (b *safeBuffer) String() string {
@@ -32,7 +38,12 @@ func (b *safeBuffer) Write(d []byte) (int, error) {
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 
-	return b.buf.Write(d)
+	n, err := b.buf.Write(d)
+	if err != nil {
+		return n, fmt.Errorf("could not write to buffer: %w", err)
+	}
+
+	return n, nil
 }
 
 func (b *safeBuffer) Reset() {
