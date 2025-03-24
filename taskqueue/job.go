16c0e8f72f87d837a42ca780a23156ea342dc1fa
Arturo Albacete <aalbacetef@gmail.com>
2025-03-24 17:02:41 +0100

    implement pipeline by satisfying tests

---
+++ taskqueue/job.go
@@ -0,0 +1,63 @@
+package taskqueue
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type JobNotFoundError struct {
+	id string
+}
+
+func (e JobNotFoundError) Error() string {
+	return fmt.Sprintf("job with id '%s' not found", e.id)
+}
+
+type JobState string
+
+const (
+	NotStarted JobState = "not-started"
+	Queued     JobState = "queued"
+	Running    JobState = "running"
+	Failed     JobState = "failed"
+	Done       JobState = "done"
+)
+
+type JobFn func(context.Context) error
+
+func NewJob(fn JobFn) (*Job, error) {
+	id := uuid.New().String()
+
+	return &Job{
+		ID:          id,
+		fn:          fn,
+		timeCreated: time.Now(),
+	}, nil
+}
+
+type Job struct {
+	ID          string
+	state       JobState
+	mu          sync.Mutex
+	timeAdded   time.Time
+	timeCreated time.Time
+	fn          JobFn
+}
+
+func (job *Job) SetState(state JobState) {
+	job.mu.Lock()
+	job.state = state
+	job.mu.Unlock()
+}
+
+func (job *Job) GetState() JobState {
+	job.mu.Lock()
+	state := job.state
+	job.mu.Unlock()
+
+	return state
+}
--- taskqueue/pipeline.go
+++ taskqueue/pipeline.go
@@ -2,101 +2,182 @@ package taskqueue
 
 import (
 	"context"
-	"fmt"
-	"sync"
-
-	"github.com/google/uuid"
+	"errors"
+	"time"
 )
 
-type JobNotFoundError struct {
-	id string
+type Event struct {
+	Type EventType
+	Job  *Job
+
+	responseCh chan<- PipelineState
 }
 
-func (e JobNotFoundError) Error() string {
-	return fmt.Sprintf("job with id '%s' not found", e.id)
-}
-
-type JobState string
+type EventType string
 
 const (
-	NotStarted JobState = "not-started"
-	Queued     JobState = "queued"
-	Running    JobState = "running"
-	Failed     JobState = "failed"
-	Done       JobState = "done"
+	JobAdded           EventType = "job-added"
+	JobEnded           EventType = "job-ended"
+	PipelineStarted    EventType = "pipeline-started"
+	PipelinePaused     EventType = "pipeline-ended"
+	QueryPipelineState EventType = "query-pipeline-state"
 )
 
-type JobFn func(context.Context) error
-
-func NewJob(fn JobFn) (*Job, error) {
-	id := uuid.NewUUID()
-
-	return &Job{ID: id}, nil
-}
-
-type Job struct {
-	ID    string
-	state JobState
-	mu    sync.Mutex
-}
-
-func (job *Job) SetState(state JobState) {
-	job.mu.Lock()
-	job.state = state
-	job.mu.Unlock()
-}
-
-func (job *Job) GetState() JobState {
-	job.mu.Lock()
-	state := job.state
-	job.mu.Unlock()
-
-	return state
-}
+const eventChanSize = 100
 
 func NewPipeline(name string) (*Pipeline, error) {
-	return &Pipeline{Name: name}, nil
+	ctx, cancel := context.WithCancel(context.Background())
+
+	pipeline := &Pipeline{
+		Name:    name,
+		cancel:  cancel,
+		eventCh: make(chan Event, eventChanSize),
+	}
+
+	go pipeline.runEventLoop(ctx)
+
+	return pipeline, nil
 }
 
 type Pipeline struct {
-	mu           sync.Mutex
 	jobs         []*Job
 	currentIndex int
 	cancel       context.CancelFunc
-	inputJobChan chan *Job
+	eventCh      chan Event
+	isStarted    bool
 
 	Name string
 }
 
 func (pipeline *Pipeline) Add(job *Job) error {
-	pipeline.mu.Lock()
-	defer pipeline.mu.Unlock()
-
-	pipeline.jobs = append(pipeline.jobs, job)
-
+	pipeline.eventCh <- Event{
+		Type: JobAdded,
+		Job:  job,
+	}
 	return nil
 }
 
+func (pipeline *Pipeline) runEventLoop(ctx context.Context) {
+	for {
+		select {
+		case event := <-pipeline.eventCh:
+			pipeline.handleEvent(ctx, event)
+
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (pipeline *Pipeline) handleEvent(ctx context.Context, event Event) {
+	switch event.Type {
+	case JobAdded:
+		job := event.Job
+		job.timeAdded = time.Now()
+		job.SetState(Queued)
+
+		pipeline.jobs = append(pipeline.jobs, job)
+		pipeline.runNextJob(ctx)
+
+	case JobEnded:
+		pipeline.runNextJob(ctx)
+
+	case QueryPipelineState:
+		state := PipelineState{
+			jobStates: make(map[string]JobState),
+		}
+
+		for _, job := range pipeline.jobs {
+			state.jobStates[job.ID] = job.GetState()
+		}
+
+		event.responseCh <- state
+
+	case PipelineStarted:
+		pipeline.isStarted = true
+
+	case PipelinePaused:
+		pipeline.isStarted = false
+		if pipeline.cancel != nil {
+			pipeline.cancel()
+			pipeline.cancel = nil
+		}
+	}
+}
+
+func (pipeline *Pipeline) runNextJob(ctx context.Context) {
+	if !pipeline.isStarted {
+		return
+	}
+
+	index := pipeline.currentIndex
+	n := len(pipeline.jobs)
+
+	if index >= (n - 1) {
+		return
+	}
+
+	current := pipeline.jobs[index]
+	state := current.GetState()
+
+	if state == Running {
+		return
+	}
+
+	if state == Queued {
+		current.SetState(Running)
+		go pipeline.execute(ctx, current)
+		return
+	}
+
+	pipeline.currentIndex++
+	job := pipeline.jobs[pipeline.currentIndex]
+	job.SetState(Running)
+	go pipeline.execute(ctx, job)
+}
+
+func (pipeline *Pipeline) execute(ctx context.Context, job *Job) {
+	err := job.fn(ctx)
+
+	job.SetState(Done)
+	if err != nil {
+		job.SetState(Failed)
+	}
+
+	pipeline.eventCh <- Event{
+		Type: JobEnded,
+	}
+}
+
 func (pipeline *Pipeline) Start() error {
+	pipeline.eventCh <- Event{
+		Type: PipelineStarted,
+	}
+	return nil
 }
 
 func (pipeline *Pipeline) Pause() error {
-
+	return nil
 }
 
-func (pipeline *Pipeline) State() PipelineState {
-	pipeline.mu.Lock()
-	defer pipeline.mu.Unlock()
+var ErrQueryPipelineTimeout = errors.New("timed out waiting for pipeline state")
 
-	state := PipelineState{
-		jobStates: make(map[string]JobState),
+func (pipeline *Pipeline) State() (PipelineState, error) {
+	const queryPipelineStateTimeout = 15 * time.Second
+
+	responseCh := make(chan PipelineState, 1)
+
+	pipeline.eventCh <- Event{
+		Type:       QueryPipelineState,
+		responseCh: responseCh,
 	}
 
-	for _, job := range pipeline.jobs {
-		state.jobStates[job.ID] = job.GetState()
+	select {
+	case <-time.After(queryPipelineStateTimeout):
+		return PipelineState{}, ErrQueryPipelineTimeout
+	case state := <-responseCh:
+		return state, nil
 	}
-
-	return state
 }
 
 type PipelineState struct {
